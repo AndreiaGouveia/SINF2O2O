e6db109e43bddfac1f8bcd438a9ded90872a9539
Andreia <andreiagouveia1@gmail.com>
2020-12-05 20:50:30 +0000

    Now its working generally

--- be/controllers/companiesController.js
+++ be/controllers/companiesController.js
@@ -1,7 +1,7 @@
 const tenant_differ = "243034/";
-const tenant_sinf = process.env.TENANT_SINF;
+const tenant_sinf = "243035/";
 const token_differ = "243034-0001/"; 
-const token_sinf = process.env.SINF; 
+const token_sinf = "243035-0001/"; 
 const url = "https://my.jasminsoftware.com/api/";
 var FormData = require("form-data");
 const { urlencoded, response } = require("express");
@@ -12,74 +12,74 @@ const db = require("../database/database");
 exports.get_companies_products = async function getCompanieProducts(req, res, next){
 
 
-    if(req.params.id != 0 && req.params.idv != 1){
+    if(req.params.id != 0 && req.params.id != 1){
         res.status(400).json({success: false, error: "There needs to be a valid id"});
         return;
     }
-
-    if(req.params.id == 0) //DIFFER
-    {
-        console.log(db)
-        let result = {};
-        db.all("SELECT * from company where id=0", function(err,rows){
-            if(err)
-            console.log(err);
-            else
-            rows.forEach(function (row) {
-              console.log("i am gehre")
-                result = row;
-                getSalesProducts(result , res);
-          }); 
+    
+    let params = [req.params.id];
+    let result = {};
+    db.all("SELECT * from company where id=$1",params, function(err,rows){
+      if(err){
+        res.status(400).json({success: false, error: "Invalid query"});
+      }
+      else
+        rows.forEach(function (row) {
+          console.log(row);
+          result = row;
+          getSalesProducts(result , res);
         }); 
-
-        
-    }else{ //SINF
-
-    }
+      }); 
 }
 
 async function getToken(result,res){
-//get token!!
-        const form = new FormData();
-        form.append("grant_type", result.grant_type);
-        form.append("client_id", result.client_id);
-        form.append("client_secret", result.client_secret);
-        form.append("scope", result.scope);
+      //get token!!
+      const form = new FormData();
+      form.append("grant_type", result.grant_type);
+      form.append("client_id", result.client_id);
+      form.append("client_secret", result.client_secret);
+      form.append("scope", result.scope);
 
-        const requestOptions = {
-            method: 'POST',
-            body : form
-        };
+      const requestOptions = {
+          method: 'POST',
+          body : form
+      };
 
-        try {
-            const response = await axios.post(
-              "https://identity.primaverabss.com/core/connect/token",
-              form,
-              { headers: { ...form.getHeaders() } }
-            );
-            const { data } = response;
-            if (response.status !== 200) {
-              return Promise.reject(data);
-            }
-            let params = [data.access_token , 0];
-            db.run("UPDATE company SET token = $1 where id = $2", params, function(err){
-              if(err){
-                console.log(err);
-              }
-              console.log("I have updated")
-            });
-            return data.access_token;
-          } catch (error) {
-            return error;
+      try {
+          const response = await axios.post(
+            "https://identity.primaverabss.com/core/connect/token",
+            form,
+            { headers: { ...form.getHeaders() } }
+          );
+          const { data } = response;
+          if (response.status !== 200) {
+            return Promise.reject(data);
           }
-          console.log("livin ma life")
-
+          let params = [data.access_token , result.id];
+          db.run("UPDATE company SET token = $1 where id = $2", params, function(err){
+            if(err){
+              res.status(400).json({success: false, error: "Invalid query"});
+            }
+            console.log("I have updated")
+          });
+          return data.access_token;
+        } catch (error) {
+          return error;
+        }
 }
 
 async function getSalesProducts(result,res){
+  let tenant = tenant_differ;
+  let company = token_differ;
+
+  if(result.id == 1){
+    tenant = tenant_sinf;
+    company = token_sinf;
+  }
+
   let token = result.token;
     axios
-          .get(url  + tenant_differ +  token_differ + "salescore/salesitems", {
+          .get(url + tenant + company + "salescore/salesitems", {
             headers: {
               Authorization: `Bearer ${token}`,
               "Content-Type": "x-www-form-urlencoded"
@@ -89,13 +89,15 @@ async function getSalesProducts(result,res){
             res.status(200).json({ success: true, result: data });
           })
           .catch(error => {
-              console.log(error);
+            res.status(400).json({success: false, error: error});
+              //console.log(error);
               //token might have expired or might not even exist
+              /*console.log("will try token");
               getToken(result).then(response => {
                 result.token =response;
                 getSalesProducts(result , res);
-              });
-              //res.status(400).json({ success: false, error: "error getting Sales Items" });
+              });*/
+
           });
 
 }

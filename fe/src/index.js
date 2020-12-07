7da711ea490e1ced46f4acf964434ec2bf6a5076
Andreia <andreiagouveia1@gmail.com>
2020-12-07 14:36:07 +0000

    Fixed merge issues

--- fe/src/index.js
+++ fe/src/index.js
@@ -7,7 +7,6 @@ import Companies from './Pages/Companies'
 import Transactions from './Pages/Transactions'
 import Inventory from './Pages/Inventory'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import EditCompanies from './Components/EditCompanies';
 ReactDOM.render(
   <React.StrictMode>
     <Router>
@@ -17,7 +16,6 @@ ReactDOM.render(
         <Route path ="/companies" exact component={Companies} />
         <Route path ="/transactions" exact component={Transactions} />
         <Route path ="/inventory" exact component={Inventory} />
-        <Route path ="/edit-companies" exact component={EditCompanies} />
         </>
       </Layout>
     </Router>

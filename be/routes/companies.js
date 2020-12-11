a83995135cded42311e390f08d22e6f726764030
Skaarfungandr <44262477+Skaarfungandr@users.noreply.github.com>
2020-12-11 04:01:07 +0000

    undid small change

--- be/routes/companies.js
+++ be/routes/companies.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const companyController = require('../controllers/companiesController');
 
 //Get Company info
-router.get('/products/:id', companyController.get_companies_sales_products);
+router.get('/products/:id', companyController.get_companies_products);
 router.get('/products/purchased/:id', companyController.get_companies_purchased_products);
 router.get('/companiesInfo',  companyController.get_companies_info);
 

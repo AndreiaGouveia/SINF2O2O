9317f68bfae728a666e62f58000f9738e2c2bb47
Andreia <andreiagouveia1@gmail.com>
2020-12-13 17:11:41 +0000

    Deleted  css

--- fe/src/index.js
+++ fe/src/index.js
@@ -6,7 +6,6 @@ import MainPage from './Pages/MainPage';
 import Companies from './Pages/Companies';
 import Transactions from './Pages/Transactions';
 import Inventory from './Pages/Inventory';
-import CreateOrder from './Pages/CreateOrder';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -19,7 +18,6 @@ ReactDOM.render(
           <Route path="/companies" exact component={Companies} />
           <Route path="/transactions" exact component={Transactions} />
           <Route path="/inventory" exact component={Inventory} />
-          <Route path="/createorder" exact component={CreateOrder} />
         </>
       </Layout>
     </Router>
